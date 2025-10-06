4f9110261a699df4a5836c0754ab9971425dadef
Priyanshu Dwivedi <118672452+Priyanshu-073@users.noreply.github.com>
2025-10-06 20:02:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move all negative elements to end/move-all-negative-elements-to-end.cpp
+++ Difficulty: Easy/Move all negative elements to end/move-all-negative-elements-to-end.cpp
@@ -13,38 +13,19 @@ class Solution {
         //         pos++;
         //     }
         // }
-        // int n =arr.size();
-        // vector<int> nums;
-        // for(int i=0;i<n;i++){
-        //     if(arr[i] > 0){
-        //         nums.push_back(arr[i]);
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     if(arr[i]<0){
-        //         nums.push_back(arr[i]);
-        //     }
-        // }
-        // arr=nums;
-         int n = arr.size();
-        vector<int> nums;  // start empty
-
-        // first store non-negative numbers
-        for (int i = 0; i < n; i++) {
-            if (arr[i] >= 0) {
+        int n =arr.size();
+        vector<int> nums;
+        for(int i=0;i<n;i++){
+            if(arr[i] >= 0){
                 nums.push_back(arr[i]);
             }
         }
-
-        // then store negatives
-        for (int i = 0; i < n; i++) {
-            if (arr[i] < 0) {
+        for(int i=0;i<n;i++){
+            if(arr[i]<0){
                 nums.push_back(arr[i]);
             }
         }
-
-        arr = nums;
-
+        arr=nums;
         
     }
 };
