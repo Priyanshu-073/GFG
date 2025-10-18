fe6fc0fe15d14d96ed25b13958ecfbbd3a225460
Priyanshu Dwivedi <118672452+Priyanshu-073@users.noreply.github.com>
2025-10-18 17:16:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    vector<int> findDuplicates(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        vector<int> ans;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n-1;i++){
+            if(arr[i] == arr[i+1]){
+                ans.push_back(arr[i]);
+            }
+        }
+        return ans;
+    }
+};
