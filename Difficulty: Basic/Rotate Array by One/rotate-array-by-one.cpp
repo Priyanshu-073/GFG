9ff8a1644ee4c48986c86ac1ca0aa47b7ef11434
Priyanshu Dwivedi <118672452+Priyanshu-073@users.noreply.github.com>
2025-12-10 17:30:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Rotate Array by One/rotate-array-by-one.cpp
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.cpp
@@ -5,12 +5,10 @@ class Solution {
     void rotate(vector<int> &arr) {
         // code here
         int n=arr.size();
-        int temp = arr[n-1];
-        for(int i=n-1;i>0;i--){
-            arr[i] = arr[i-1];
+        int temp=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            arr[i+1]=arr[i];
         }
-        arr[0] = temp;
-        
-        
+        arr[0]=temp;
     }
 };
